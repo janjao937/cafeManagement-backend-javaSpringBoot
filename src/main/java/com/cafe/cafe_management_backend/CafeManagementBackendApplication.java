bb9edc083462d9f3fb180448f0e01513583c667a
janjao937 <janjao937@gmail.com>
2024-10-22 04:58:52 +0700

    init git

---
+++ src/main/java/com/cafe/cafe_management_backend/CafeManagementBackendApplication.java
@@ -0,0 +1,14 @@
+package com.cafe.cafe_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManagementBackendApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(CafeManagementBackendApplication.class, args);
+	}
+
+}
